f11b2d1ad5403a1b963b2b56f129338841f47be7
Wahyu Anas <wahyuanas5554@gmail.cpm>
2022-06-12 09:06:02 +0700

    edit pos_db.sql file

--- pos_db.sql
+++ pos_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2022 at 11:08 AM
+-- Generation Time: Jun 12, 2022 at 04:04 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -27,10 +27,14 @@ CREATE TABLE IF NOT EXISTS `tbl_account` (
   `email` varchar(256) NOT NULL,
   `address` varchar(500) NOT NULL,
   `business_type` tinyint(4) NOT NULL,
+  `main_business_type` tinyint(4) DEFAULT NULL,
+  `core_business_type` varchar(200) NOT NULL,
   `outlets_number` smallint(6) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `phone_number` (`phone_number`),
   KEY `email` (`email`),
   KEY `company_name` (`company_name`),
-  KEY `business_type` (`business_type`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  KEY `business_type` (`business_type`),
+  KEY `core_business_type` (`core_business_type`),
+  KEY `main_business_type` (`main_business_type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=112 ;
